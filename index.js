f7bc0f391c5d0c4b6c834519024cb6d66bba9f94
ramdev.io.id@gmail.com <ramdev.io.id@gmail.com>
2023-02-27 17:04:06 +0700

    test6775

--- index.js
+++ index.js
@@ -36,10 +36,9 @@ app.get('/download/', async (req, res) => {
 })
 
 app.get('/down/', async (req, res) => {
-    let url = req.params.url;
+    let data = req.params.data;
     let type = req.params.type;
-    const rtik = await RTikDown(url);
-    console.log(rtik)/*
+    
     function strRandom(length) {
     var result = '';
     var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
@@ -55,7 +54,7 @@ app.get('/down/', async (req, res) => {
       let id = 'RTik_'+str
       const path = process.cwd() + `/temp/media/${type}/${id}.mp4`;
       
-      const requ = https.get(rtik.video.noWatermark, (response) => {
+      const requ = https.get(data, (response) => {
         const file = fs.createWriteStream(path);
         response.pipe(file);
         file.on("error", function(err) {
@@ -76,7 +75,7 @@ app.get('/down/', async (req, res) => {
       let rid = strRandom(5);
       let id = 'RTik_' + rid
       const path = process.cwd() + `/temp/media/${type}/${id}.mp3`;
-      https.get(rtik.music.play_url, (response) => {
+      https.get(data, (response) => {
         const file = fs.createWriteStream(path);
         response.pipe(file);
         file.on("error", function(err) {
@@ -90,7 +89,7 @@ app.get('/down/', async (req, res) => {
           console.log("done");
         });
       });
-    }*/
+    }
 });
 
 
