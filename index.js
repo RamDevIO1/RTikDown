353fe4c8973f405536a979c4764c12de8f8931f4
ramdev.io.id@gmail.com <ramdev.io.id@gmail.com>
2023-03-02 18:45:31 +0700

    ....m

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const ejs = require('ejs');
 
 function RTikDown(url) {
   return new Promise((resolve, reject) => {
-    axios.get(`https://developers.tiklydown.me/api/download?url=${url}`)
+    axios.get(`https://www.tikwm.com/api/?url=${url}`)
       .then(({ data }) => {
         resolve(data)
       })
@@ -17,35 +17,6 @@ function RTikDown(url) {
   })
 }
 
-async function testtikwm(url){
-let domain = 'https://www.tikwm.com/';
-let res = await axios.post(domain + 'api/', {}, {
-  headers: {
-    'accept': 'application/json, text/javascript, */*; q=0.01',
-    'content-type': 'application/x-www-form-urlencoded; charset=UTF-8',
-    // 'cookie': 'current_language=en; _ga=GA1.1.115940210.1660795490; _gcl_au=1.1.669324151.1660795490; _ga_5370HT04Z3=GS1.1.1660795489.1.1.1660795513.0.0.0',
-    'sec-ch-ua': '"Chromium";v="104", " Not A;Brand";v="99", "Google Chrome";v="104"',
-    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36'
-  },
-  params: {
-    url: url,
-    
-  }
-})
-
-return {
-  data: res.data,
-  nowm: domain + res.data.data.play,
-  wm: domain + res.data.data.wmplay,
-  music: domain + res.data.data.music,
-}
-}
-async function abcs(){
-  let data = await testtikwm("https://vm.tiktok.com/ZS89TgsEW/")
-  console.log(data)
-
-}
-abcs()
 function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
@@ -91,7 +62,7 @@ app.get('/down/', async (req, res) => {
   const rtik = await RTikDown(url);
   if (type == "mp4") {
     const path = process.cwd() + `/temp/media/${type}/${id}.mp4`;
-    const requ = https.get(rtik.video.noWatermark, (response) => {
+    const requ = https.get(rtik.data.play, (response) => {
       const file = fs.createWriteStream(path);
       response.pipe(file);
       file.on("error", function(err) {
@@ -108,7 +79,7 @@ app.get('/down/', async (req, res) => {
     });
   } else if (type == "mp3") {
     const path = process.cwd() + `/temp/media/${type}/${id}.mp3`;
-    const requ = https.get(rtik.music.play_url, (response) => {
+    const requ = https.get(rtik.data.music, (response) => {
       const file = fs.createWriteStream(path);
       response.pipe(file);
       file.on("error", function(err) {
@@ -125,13 +96,6 @@ app.get('/down/', async (req, res) => {
     });
   }
 })
-
-
-
-async function savemp4(rtik_data, res, req) {
-  
-}
-
 
 app.get('/rdown/:type/:id', (req, res) => {
   let type = req.params.type;
